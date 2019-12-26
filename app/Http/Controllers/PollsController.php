8eda96c2473f8bbda1db647de3114a6965ce2589
Omarabdelfattah1 <omar.abdelfattah.shehata@gmail.com>
2019-12-26 14:19:06 +0200

    CRUD created,validation,error handling

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Poll;
+use Validator;
+class PollsController extends Controller
+{
+    public function index()
+    {
+    	return response()->json(Poll::get(),200);
+    }
+
+    public function show($id)
+    {
+    	$poll=Poll::find($id);
+    	if (is_null($poll)) {
+    		return response()->json(null,404);
+    	}
+
+    	return response()->json($poll,200);
+    }
+
+    public function store(Request $request)
+    {
+    	$rules=[
+    		'title'=>'required|max:225'
+    	];
+    	$Validator=Validator::make($request->all(),$rules);
+    	if ($Validator->fails()) {
+    		return response()->json($Validator->errors(),400);
+    	}
+    	$poll=Poll::create($request->all());
+    	return response()->json($poll,201);
+    }
+
+    public function update(Poll $poll,Request $request)
+    {
+    	$poll->update($request->all());
+    	return response()->json($poll,200);
+    }
+
+    public function destroy(Poll $poll)
+    {
+    	$poll->delete();
+    	return response()->json(["Deleted"],204);
+    }
+}
